5ae1cae4ba358b51ac933cdbb45bb5a825e1a6ab
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-30 22:28:52 +0300

    task4

---
+++ Tyuiu.EgorovAD.Sprint3.Task4.V2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("1 шаг:" + startValue);
+            Console.WriteLine("2 шаг:" + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Вывод:"+ ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
