046a38c6ebdd49b3d13ebc207dfa1e320ea45aab
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-30 15:40:53 +0300

    Task1

---
+++ Tyuiu.EgorovAD.Sprint3.Task1.V26.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.EgorovAD.Sprint3.Task1.V26.Lib
+{
+    public class DataService : ISprint3Task1V26
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double jut = 0;
+            while (startValue <= stopValue)
+            {
+                jut += Math.Pow(6 / Math.Pow(value, startValue), 2);
+                startValue++;
+            }
+            return Math.Round(jut, 3);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint3.Task1.V26/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task1.V26.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task1.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 8;
+            
+
+            Console.WriteLine("Переменная x = " + value);
+            Console.WriteLine("от  " + startValue);
+            Console.WriteLine("до  " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ответ:" + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
