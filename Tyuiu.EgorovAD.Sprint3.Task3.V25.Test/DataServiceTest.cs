67f426b3d50c3041635390b7a77fc1ebf4c076e9
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-30 21:40:25 +0300

    task3

---
+++ Tyuiu.EgorovAD.Sprint3.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task3.V25.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "tughghurghhu";
+            char chr = 'u';
+
+            double res = ds.GetMinCharCount(str, chr);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint3.Task3.V25/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task3.V25.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task3.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = " kjhgfuu gujut dtuu";
+            char chr = 'u';
+            Console.WriteLine("Исходная строка:" + value);
+            Console.WriteLine("Исходный символ:" + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* Колво символов " + ds.GetMinCharCount(value, chr));
+            Console.ReadKey();  
+        }
+    }
+}
