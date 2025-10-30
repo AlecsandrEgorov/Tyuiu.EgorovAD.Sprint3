cdbf0c931b60455747389f91bc5246e7c66e2fa4
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-30 16:50:17 +0300

    Task1

---
+++ Tyuiu.EgorovAD.Sprint3.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task1.V26.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 8;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 1.5;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint3.Task1.V26/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task1.V26.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task1.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value = 5;
+            int st = 1;
+            int end = 8;
+            Console.WriteLine("Переменная x = " + value);
+            Console.WriteLine("от  " + st);
+            Console.WriteLine("до  " + end);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ответ:" + ds.GetSumSeries(value, st, end));
+            Console.ReadKey();
+        }
+    }
+}
