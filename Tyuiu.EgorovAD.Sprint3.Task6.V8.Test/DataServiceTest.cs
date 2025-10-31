2ca7575c2b858fdb2888c10d8050b0e1d5ea4ed0
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-31 18:47:30 +0300

    Task6

---
+++ Tyuiu.EgorovAD.Sprint3.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task6.V8.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task6.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 18;
+            int stopValue = 28;
+            double res = ds.GetSumTheDivisors(startValue, stopValue);
+            double wait = 30;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint3.Task6.V8/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.EgorovAD.Sprint3.Task6.V8.Lib;
+namespace Tyuiu.EgorovAD.Sprint3.Task6.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 18;
+            int stopValue = 28;
+            Console.WriteLine("Начало отрезка:" + startValue);
+            Console.WriteLine("КОНЕЦ отрезка:" + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Вывод:" + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
